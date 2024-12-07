353eaed3ad7abd8544ced965348c2293814df97c
sachin <kadamsachin2006@gmail.com>
2024-12-07 14:08:30 +0530

    Ccha

--- products.js
+++ products.js
@@ -4,56 +4,56 @@ const products = [
         "name":"KURTY01",
         "price": 200,
         "image": "images/1.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "description": "Our Clothes are combine style, comfort, and functionality to suit various occasions and preferences. They are crafted from diverse fabrics like cotton, polyester, or blends, offering options for casual, formal, or athletic wear. Designs cater to trends, featuring unique patterns, colors, and cuts to enhance personal expression. Quality and durability are key, ensuring a lasting and satisfying experience."
     },
     {
         "id": 2,
         "name":"KURTY02",
         "price": 250,
         "image": "images/2.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "description": "Our Clothes are combine style, comfort, and functionality to suit various occasions and preferences. They are crafted from diverse fabrics like cotton, polyester, or blends, offering options for casual, formal, or athletic wear. Designs cater to trends, featuring unique patterns, colors, and cuts to enhance personal expression. Quality and durability are key, ensuring a lasting and satisfying experience."
     },
     {
         "id": 3,
         "name":"KURTY03",
         "price": 290,
         "image": "images/3.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "description": "Our Clothes are combine style, comfort, and functionality to suit various occasions and preferences. They are crafted from diverse fabrics like cotton, polyester, or blends, offering options for casual, formal, or athletic wear. Designs cater to trends, featuring unique patterns, colors, and cuts to enhance personal expression. Quality and durability are key, ensuring a lasting and satisfying experience."
     },
     {
         "id": 4,
         "name":"KURTY04",
         "price": 200,
         "image": "images/4.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "description": "Our Clothes are combine style, comfort, and functionality to suit various occasions and preferences. They are crafted from diverse fabrics like cotton, polyester, or blends, offering options for casual, formal, or athletic wear. Designs cater to trends, featuring unique patterns, colors, and cuts to enhance personal expression. Quality and durability are key, ensuring a lasting and satisfying experience."
     },
     {
         "id": 5,
         "name":"KURTY05",
         "price": 300,
         "image": "images/5.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "description": "Our Clothes are combine style, comfort, and functionality to suit various occasions and preferences. They are crafted from diverse fabrics like cotton, polyester, or blends, offering options for casual, formal, or athletic wear. Designs cater to trends, featuring unique patterns, colors, and cuts to enhance personal expression. Quality and durability are key, ensuring a lasting and satisfying experience."
     },
     {
         "id": 6,
         "name":"KURTY06",
         "price": 200,
         "image": "images/6.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "description": "Our Clothes are combine style, comfort, and functionality to suit various occasions and preferences. They are crafted from diverse fabrics like cotton, polyester, or blends, offering options for casual, formal, or athletic wear. Designs cater to trends, featuring unique patterns, colors, and cuts to enhance personal expression. Quality and durability are key, ensuring a lasting and satisfying experience."
     },
     {
         "id": 7,
         "name":"KURTY07",
         "price": 200,
         "image": "images/7.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "description": "Our Clothes are combine style, comfort, and functionality to suit various occasions and preferences. They are crafted from diverse fabrics like cotton, polyester, or blends, offering options for casual, formal, or athletic wear. Designs cater to trends, featuring unique patterns, colors, and cuts to enhance personal expression. Quality and durability are key, ensuring a lasting and satisfying experience."
     },
     {
         "id": 8,
         "name":"KURTY08",
         "price": 200,
         "image": "images/8.png",
-        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+        "description": "Our Clothes are combine style, comfort, and functionality to suit various occasions and preferences. They are crafted from diverse fabrics like cotton, polyester, or blends, offering options for casual, formal, or athletic wear. Designs cater to trends, featuring unique patterns, colors, and cuts to enhance personal expression. Quality and durability are key, ensuring a lasting and satisfying experience."
     }
 
 ];
